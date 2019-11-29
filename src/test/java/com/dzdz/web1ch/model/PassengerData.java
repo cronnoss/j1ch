c2dc760c8b9ce0106a7f694fa41d9b621b7ba60c
cronnoss <karasevevg@gmail.com>
2019-11-29 15:47:35 +1000

    add PassengerCreationTests

---
+++ src/test/java/com/dzdz/web1ch/model/PassengerData.java
@@ -0,0 +1,62 @@
+package com.dzdz.web1ch.model;
+
+public class PassengerData {
+    private final String firstName;
+    private final String lastName;
+    private final String birthDate;
+    private final String citizenshipId;
+    private final String residenceId;
+    private final String passportNum;
+    private final String passportIssueCountryId;
+    private final String passportIssueDate;
+    private final String passportExpiryDate;
+
+
+    public PassengerData(String firstName, String lastName, String birthDate, String citizenshipId, String residenceId, String passportNum, String passportIssueCountryId, String passportIssueDate, String passportExpiryDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.citizenshipId = citizenshipId;
+        this.residenceId = residenceId;
+        this.passportNum = passportNum;
+        this.passportIssueCountryId = passportIssueCountryId;
+        this.passportIssueDate = passportIssueDate;
+        this.passportExpiryDate = passportExpiryDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public String getCitizenshipId() {
+        return citizenshipId;
+    }
+
+    public String getResidenceId() {
+        return residenceId;
+    }
+
+    public String getPassportNum() {
+        return passportNum;
+    }
+
+    public String getPassportIssueCountryId() {
+        return passportIssueCountryId;
+    }
+
+    public String getPassportIssueDate() {
+        return passportIssueDate;
+    }
+
+    public String getPassportExpiryDate() {
+        return passportExpiryDate;
+    }
+}
---
+++ src/test/java/com/dzdz/web1ch/tests/PassengerCreationTests.java
@@ -0,0 +1,15 @@
+package com.dzdz.web1ch.tests;
+
+import com.dzdz.web1ch.model.PassengerData;
+import org.junit.Test;
+
+public class PassengerCreationTests extends TestJUnitTestBase {
+
+    @Test
+    public void testPassengerCreation() throws InterruptedException {
+        app.gotoPassengersPage();
+        app.initPassengerCreation();
+        app.fillPassengerForm( new PassengerData("Daniel", "Zagar","19800505", "Slovenia", "Slovenia", "PB1258535","Slovenia","20140216","20240215"));
+        app.submitPassengerCreation();
+    }
+}
