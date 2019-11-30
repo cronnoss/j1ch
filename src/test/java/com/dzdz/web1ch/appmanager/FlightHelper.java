6705aa68284348b7f87a55cf28db6ef3b149fc9e
cronnoss <karasevevg@gmail.com>
2019-11-30 12:52:21 +1000

    improved method fillFlightForm for subsequent allocation of methods

--- src/test/java/com/dzdz/web1ch/appmanager/FlightHelper.java
+++ src/test/java/com/dzdz/web1ch/appmanager/FlightHelper.java
@@ -23,31 +23,44 @@ public class FlightHelper {
     public void fillFlightForm(FlightData flightData) throws InterruptedException {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         driver.findElement(By.name("booking_ref")).click();
+        driver.findElement(By.name("booking_ref")).clear();
+        Thread.sleep(1000);
         driver.findElement(By.name("booking_ref")).sendKeys(flightData.getPnr());
 
         driver.findElement(By.name("airline_code")).click();
+        driver.findElement(By.name("airline_code")).clear();
         Thread.sleep(1000);
         driver.findElement(By.name("airline_code")).sendKeys(flightData.getAirlineCode());
         Thread.sleep(4000);
         driver.findElement(By.name("airline_code")).sendKeys(Keys.DOWN);
         driver.findElement(By.name("airline_code")).sendKeys(Keys.ENTER);
 
-        driver.findElement(By.cssSelector(".obj-flight")).click();
+        driver.findElement(By.name("flight_num")).click();
+        driver.findElement(By.name("flight_num")).clear();
+        Thread.sleep(1000);
         driver.findElement(By.name("flight_num")).sendKeys(flightData.getFlightNum());
 
-        driver.findElement(By.name("flight_date")).sendKeys(flightData.getDate());
+        driver.findElement(By.name("flight_date")).click();
+        driver.findElement(By.name("flight_date")).clear();
         Thread.sleep(1000);
+        driver.findElement(By.name("flight_date")).sendKeys(flightData.getDate());
 
-        driver.findElement(By.name("flight_time")).sendKeys(Keys.ENTER);
+        driver.findElement(By.name("flight_time")).click();
         driver.findElement(By.name("flight_time")).clear();
+        Thread.sleep(1000);
         driver.findElement(By.name("flight_time")).sendKeys(flightData.getFlightTime());
 
-        driver.findElement(By.name("arrival_time")).sendKeys(Keys.ENTER);
+        driver.findElement(By.name("arrival_time")).click();
         driver.findElement(By.name("arrival_time")).clear();
+        Thread.sleep(1000);
         driver.findElement(By.name("arrival_time")).sendKeys(flightData.getArrivalTime());
 
         js.executeScript("window.scrollBy(0,100)");
         Thread.sleep(3000);
+
+        driver.findElement(By.name("departure_code")).click();
+        driver.findElement(By.name("departure_code")).clear();
+        Thread.sleep(1000);
         driver.findElement(By.name("departure_code")).sendKeys(flightData.getDeparture());
         Thread.sleep(3000);
         driver.findElement(By.name("departure_code")).sendKeys(Keys.DOWN);
@@ -55,6 +68,9 @@ public class FlightHelper {
         driver.findElement(By.name("departure_code")).sendKeys(Keys.ENTER);
         Thread.sleep(2000);
 
+        driver.findElement(By.name("destination_code")).click();
+        driver.findElement(By.name("destination_code")).clear();
+        Thread.sleep(1000);
         driver.findElement(By.name("destination_code")).sendKeys(flightData.getDestination());
         Thread.sleep(3000);
         driver.findElement(By.name("destination_code")).sendKeys(Keys.DOWN);
@@ -64,11 +80,16 @@ public class FlightHelper {
         js.executeScript("window.scrollBy(0,300)");
         Thread.sleep(2000);
         driver.findElement(By.xpath("//button[contains(.,' Add new passenger')]")).click();
-
         Thread.sleep(1000);
+
         driver.findElement(By.name("first_name")).click();
+        driver.findElement(By.name("first_name")).clear();
+        Thread.sleep(1000);
         driver.findElement(By.name("first_name")).sendKeys(flightData.getFirstName());
 
+        driver.findElement(By.name("last_name")).click();
+        driver.findElement(By.name("last_name")).clear();
+        Thread.sleep(1000);
         driver.findElement(By.name("last_name")).sendKeys(flightData.getLastName());
         Thread.sleep(1000);
     }
