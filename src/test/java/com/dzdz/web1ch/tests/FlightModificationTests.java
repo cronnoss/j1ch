92bf49eeff4ce939e734d8895cd785bab4cce14e
cronnoss <karasevevg@gmail.com>
2019-12-07 12:58:21 +1000

    provided verification and fulfillment of preconditions for Modification of flight

--- src/test/java/com/dzdz/web1ch/tests/FlightModificationTests.java
+++ src/test/java/com/dzdz/web1ch/tests/FlightModificationTests.java
@@ -10,6 +10,9 @@ public class FlightModificationTests extends TestJUnitTestBase {
     @Test
     public void testFlightModification() throws InterruptedException, AWTException {
         app.getNavigationHelper().gotoHome();
+        if (!app.getFlightHelper().isThereAFlight()) {
+            app.getFlightHelper().createFlight(new FlightData(randomeString(), "LH", randomeNum(), "20200331", "1700", "1800", "TXL", "JFK", "Greta", "Garbo", null), true);
+        }
         app.getFlightHelper().openFlightForEditing();
         app.getFlightHelper().fillFlightForm(new FlightData(randomeString(), "LH", randomeNum(), "20200430", "2000", "2100", "TXL", "JFK", "Mark", "Zuckerberg", null), false);
         app.getFlightHelper().submitSaveFlight();
