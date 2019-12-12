c5187318142b3089ebb81825919e80eaf5c83fd5
cronnoss <karasevevg@gmail.com>
2019-12-12 15:42:26 +1000

    try6 add Remote chromeDriver from markhobson/maven-chrome

--- src/test/java/com/dzdz/web1ch/appmanager/ApplicationManager.java
+++ src/test/java/com/dzdz/web1ch/appmanager/ApplicationManager.java
@@ -2,6 +2,7 @@ package com.dzdz.web1ch.appmanager;
 
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import ru.stqa.selenium.factory.WebDriverPool;
@@ -15,8 +16,8 @@ public class ApplicationManager {
     public static String baseUrl;
     public static Capabilities capabilities;
 
-    //public WebDriver driver;
-    public RemoteWebDriver driver;
+    public WebDriver driver;
+    //public RemoteWebDriver driver;
     private SessionHelper sessionHelper;
     private FlightHelper flightHelper;
     private NavigationHelper navigationHelper;
@@ -25,9 +26,12 @@ public class ApplicationManager {
     public void init() throws InterruptedException, MalformedURLException {
         //driver = WebDriverPool.DEFAULT.getDriver(gridHubUrl, capabilities);
 
-        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        /*DesiredCapabilities dc = DesiredCapabilities.chrome();
         URL url = new URL("http://localhost:4444/wd/hub/");
-        driver = new RemoteWebDriver(url, dc);
+        driver = new RemoteWebDriver(url, dc);*/
+
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        driver = new ChromeDriver();
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
