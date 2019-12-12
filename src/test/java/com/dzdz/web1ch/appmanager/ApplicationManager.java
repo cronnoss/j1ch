15fc563be93b55857c6076c5be8e0898b5c40b47
cronnoss <karasevevg@gmail.com>
2019-12-12 19:18:36 +1000

    try10 add Drivers folder to repository

--- src/test/java/com/dzdz/web1ch/appmanager/ApplicationManager.java
+++ src/test/java/com/dzdz/web1ch/appmanager/ApplicationManager.java
@@ -12,26 +12,31 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-    public static URL gridHubUrl;
+    public static URL gridHubUrl = null;
     public static String baseUrl;
     public static Capabilities capabilities;
 
-    //public WebDriver driver;
-    public RemoteWebDriver driver;
+    public WebDriver driver;
+    /*public RemoteWebDriver driver;*/
     private SessionHelper sessionHelper;
     private FlightHelper flightHelper;
     private NavigationHelper navigationHelper;
     private PassengerHelper passengerHelper;
 
     public void init() throws InterruptedException, MalformedURLException {
-        //driver = WebDriverPool.DEFAULT.getDriver(gridHubUrl, capabilities);
+        /*driver = WebDriverPool.DEFAULT.getDriver(gridHubUrl, capabilities);*/
 
-        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        /*DesiredCapabilities dc = DesiredCapabilities.chrome();
         URL url = new URL("http://localhost:4444/wd/hub/");
-        driver = new RemoteWebDriver(url, dc);
+        driver = new RemoteWebDriver(url, dc);*/
 
         /*System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         driver = new ChromeDriver();*/
+
+        /*System.setProperty("webdriver.chrome.driver", "Drivers/win/chromedriver.exe");*/
+
+        System.setProperty("webdriver.chrome.driver", "Drivers/lin/chromedriver");
+        driver = new ChromeDriver();
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
